bf51a021deffed603507812b286deae7fba3587c
Michael Bierbaumer <mb@michaelb.at>
2020-01-19 13:16:26 +0100

    added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps : [{
+    name: 'Massago',
+    script: 'node server/index.js',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    args: '',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'one',
+      host : 'www.massago.at',
+      ref  : 'origin/master',
+      repo : 'https://github.com/michaelb87/massago.at.git',
+      path : '/var/www/massago-at',
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
